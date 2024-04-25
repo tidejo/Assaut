279a9ac504101288b72034fce2f58690685696e3
mitkampert <m.kampert@protonmail.com>
2024-04-25 20:07:49 +0200

    added collisions and speed and velocity vectors

--- pyglet_v1.py
+++ pyglet_v1.py
@@ -2,8 +2,10 @@ import pyglet
 from pyglet import shapes
 import time
 import random
+import math
 
 START_TIME = time.time()
+PI = math.pi
 
 key_states = {  # Dict which sets key states
     pyglet.window.key.W: False,
@@ -20,26 +22,26 @@ key_states = {  # Dict which sets key states
 class Player:
     """Creates player class"""
     def __init__(self, x, y, color):
-        self.position_x = x
-        self.position_y = y
-        self.velocity_x = 0
-        self.velocity_y = 0
-        self.acceleration = 0.1
+        self.position = [x, y]
+        self.velocity = [0, 0]
+        self.thrust = 0.1
+        self.mass = 50
         self.width = 50
         self.height = 50
         self.color = color
         self.health = 100
     
     def draw(self):
-        shapes.Rectangle(self.position_x, self.position_y, self.width, self.height, color=self.color).draw()
+        shapes.Rectangle(self.position[0], self.position[1], self.width, self.height, color=self.color).draw()
     
-    def accelerate(self, x, y):
-        self.velocity_x += self.acceleration * x
-        self.velocity_y += self.acceleration * y
+    def accelerate(self, dir):
+        self.velocity[0] += self.thrust * math.cos(dir)
+        self.velocity[1] += self.thrust * math.sin(dir)
 
     def move(self):
-        self.position_x += self.velocity_x
-        self.position_y += self.velocity_y
+        self.position[0] += self.velocity[0]
+        self.position[1] += self.velocity[1]
+
 
 class Shot:
     pass
@@ -95,28 +97,47 @@ def display_text():
         return explanation_game.draw()
     if time.time() > START_TIME+6 and not time.time() > START_TIME+14:
         return tijn_dikke_flikker.draw()
+    
 
 
-def move(dt):
-    if key_states[pyglet.window.key.W]:
-        player1.accelerate(0,1)
-    if key_states[pyglet.window.key.S]:
-        player1.accelerate(0,-1)
-    if key_states[pyglet.window.key.A]:
-        player1.accelerate(-1,0)
+def collision(object1, object2):
+    if abs(object1.position[0] - object2.position[0]) <= (object1.width/2 + object2.width/2) and abs(object1.position[1] - object2.position[1]) <= (object1.height/2 + object2.height/2):
+        print("collision")
+        v1 = object1.velocity.copy()
+        v2 = object2.velocity.copy()
+        mass_ratio1 = object1.mass/(object1.mass + object2.mass)
+        mass_ratio2 = object2.mass/(object1.mass + object2.mass)
+        if abs(object1.position[0] - object2.position[0]) >= abs(object1.position[1] - object2.position[1]):
+            object1.velocity[0] = object1.velocity[0] - ((v1[0] - v2[0])*mass_ratio2*2)
+            object2.velocity[0] = object2.velocity[0] + ((v1[0] - v2[0])*mass_ratio1*2)
+        
+        if abs(object1.position[0] - object2.position[0]) <= abs(object1.position[1] - object2.position[1]):
+            object1.velocity[1] = object1.velocity[1] - ((v1[1] - v2[1])*mass_ratio2*2)
+            object2.velocity[1] = object2.velocity[1] + ((v1[1] - v2[1])*mass_ratio1*2)
+
+
+def update(dt):
     if key_states[pyglet.window.key.D]:
-        player1.accelerate(1,0)
-    if key_states[pyglet.window.key.UP]:
-        player2.accelerate(0,1)
-    if key_states[pyglet.window.key.DOWN]:
-        player2.accelerate(0,-1)
-    if key_states[pyglet.window.key.LEFT]:
-        player2.accelerate(-1,0)
+        player1.accelerate(0)
+    if key_states[pyglet.window.key.W]:
+        player1.accelerate(0.5*PI)
+    if key_states[pyglet.window.key.A]:
+        player1.accelerate(PI)
+    if key_states[pyglet.window.key.S]:
+        player1.accelerate(1.5*PI)
     if key_states[pyglet.window.key.RIGHT]:
-        player2.accelerate(1,0)
+        player2.accelerate(0)
+    if key_states[pyglet.window.key.UP]:
+        player2.accelerate(0.5*PI)
+    if key_states[pyglet.window.key.LEFT]:
+        player2.accelerate(PI)
+    if key_states[pyglet.window.key.DOWN]:
+        player2.accelerate(1.5*PI)
         
     player1.move()
     player2.move()
+
+    collision(player1, player2)
 
 
 window = pyglet.window.Window(1000, 800)
@@ -150,6 +171,5 @@ def on_draw():
         obstacle.draw()
 
 # Schedules the update function to be called every frame
-pyglet.clock.schedule_interval(move, 1/60.0)
-
+pyglet.clock.schedule_interval(update, 1/144.0)
 pyglet.app.run()
